2f10d68d6fdbd3689482b23cb708bdbf5bb0cfc4
slava <slafa@tut.by>
2022-12-02 23:34:50 +0300

    вторая задача по нахождению максимального числа

--- Program.cs
+++ Program.cs
@@ -1,16 +1,14 @@
 ﻿Console.WriteLine("Введите числа");
 string A = Console.ReadLine()!;
 string B = Console.ReadLine()!;
-int ParsedInput = int.Parse(A);
-int ParsedInput1 = int.Parse(B);
+string C = Console.ReadLine()!;
+int ParsedInputA = int.Parse(A);
+int ParsedInputB = int.Parse(B);
+int ParsedInputC = int.Parse(C);
+int max = ParsedInputA;
+if (ParsedInputA > max) max = ParsedInputA;
+if (ParsedInputB > max) max = ParsedInputB;
+if (ParsedInputC > max) max = ParsedInputC;
 
-if (ParsedInput > ParsedInput1)
-{
-    Console.WriteLine(A);
-
-}
-else
-{
-    Console.WriteLine(B);
-}
+Console.WriteLine(max);
 
