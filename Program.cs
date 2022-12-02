9b648c9d3b3e60d9cd0b97be7bc3c7ce14fb03d8
slava <slafa@tut.by>
2022-12-02 23:06:31 +0300

    первая задача по нахождению максимального числа

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите числа");
+string A = Console.ReadLine()!;
+string B = Console.ReadLine()!;
+int ParsedInput = int.Parse(A);
+int ParsedInput1 = int.Parse(B);
+
+if (ParsedInput > ParsedInput1)
+{
+    Console.WriteLine(A);
+
+}
+else
+{
+    Console.WriteLine(B);
+}
+
